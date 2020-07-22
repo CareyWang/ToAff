b8866a5b14aa9447cfb926e2b1c734b7bc9131c5
CareyWong <xidiancc@gmail.com>
2020-07-22 23:51:04 +0800

    Add CI

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go mod tidy
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/linux-amd64-toaff.service main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64-toaff.service main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/windows-amd64-toaff.service main.go
